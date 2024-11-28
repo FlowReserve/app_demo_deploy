6a064bea4960be38fb4a24d5dad9e64ae30ef551
agustindasilvaR <agustindasilva@gmail.com>
2024-11-28 16:43:27 +0100

    changed db engine

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ import os
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'mysecretkey')
-    SQLALCHEMY_DATABASE_URI = 'mysql://flaskuser:flaskpassword@mariadb/flaskdb'
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///site.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'myjwtsecretkey')  # Agrega esta línea
